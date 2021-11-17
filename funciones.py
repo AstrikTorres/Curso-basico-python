ecd47cfcba2b1899977f0e2759d756e0e2afe421
Astrik Torres <astriktorres@gmail.com>
2021-11-17 12:48:29 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,39 @@
+"""
+Funciones: codigo que puedes reutilizar.
+
+Las funciones se construyen de la siguiente manera:
+
+def nombre_funcion():
+    desarrollo de la funcion.
+
+Los nombres de las funciones se escriben siempre en minusculas,
+nunca con numeros y siempre se separan con guion bajo.
+
+Parametros: variables que voy a tener disponibles para usarla dentro de la función.
+"""
+
+
+def imprimir_mensaje():
+    print('Mensaje especial: ')
+    print('¡Estoy aprendiendo a usar funciones!')
+
+
+imprimir_mensaje()
+
+
+def conversacion(mensaje):
+    print('Hola')
+    print('¿Cómo estás?')
+    print(mensaje)
+    print('Adios')
+
+
+opcion = int(input('Elige una opción (1, 2, 3): '))
+if opcion == 1:
+    conversacion('Elegiste la opción 1')
+elif opcion == 2:
+    conversacion('Elegiste la opción 2')
+elif opcion == 3:
+    conversacion('Elegiste la opción 3')
+else:
+    print('Escribe una opción valida')
